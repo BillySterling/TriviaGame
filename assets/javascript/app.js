bf5d462e0bdc04f13c711263fc2c85c6eedf094a
Billy Sterling <44212511+BillySterling@users.noreply.github.com>
2019-01-30 16:27:19 -0500

    updated to use object for trivia Q&A

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
   
   $("body").on("click", ".answer", function(event){
       gameAnswer = $(this).text();
-      if (gameAnswer === correctAnswer[questionNum]) {
+      if (gameAnswer === gameArray.correctAnswer[questionNum]) {
           clearInterval(timeClock);
           gameWin();
       }
@@ -31,42 +31,16 @@ $(document).ready(function () {
   });
   
 });
-
-var questionsArray = [
-  "What is Captain Kirk's middle name?",
-  "What is the name of the ship in the show?",
-  "Which warring and Vulcan-like people is the Federation separated from by a neutral zone?",
-  "What is Ensign Chekov's first name?",
-  "What furry creatures nearly overwhelmed the Enterprise?",
-  "Which cast member was born in Toccoa Georgia?",
-  "What actress played Spock's mother?",
-  "The main enery weapons were called?",
-  "What is Chief Engineer Scott's first name?"
-];
-
-var answersArray = [
-  ["Jonathan", "Tiberius", "Kelley", "Montgomery"],
-  ["Excelsior", "Yorktown", "Yamoto", "Enterprise"],
-  ["Romulans", "Ferengi", "Cylons", "Daveks"],
-  ["Pavel", "Vladimir", "Rasputin", "Leo"],
-  ["Ewoks", "Furbys", "Tribbles", "Urchins"],
-  ["DeForest Kelley","Nichelle Nichols","Jimmie Doohan","George Takei"],
-  ["Christine Lahti","Betty White","Jane Mansfield","Jane Wyatt"],
-  ["Lasers","Phasers","Ion Cannons","Disruptors"],
-  ["Montgomery","Gentry","George","Christopher"]
-];
-
-var correctAnswer = [
-  "B. Tiberius",
-  "D. Enterprise",
-  "A. Romulans",
-  "A. Pavel",
-  "C. Tribbles",
-  "A. DeForest Kelley",
-  "D. Jane Wyatt",
-  "B. Phasers",
-  "A. Montgomery"
-];
+// 9 trivia questions
+var gameArray = {
+    questions: [
+        "What is Captain Kirk's middle name?","What is the name of the ship in the show?","Which warring and Vulcan-like people is the Federation separated from by a neutral zone?","What is Ensign Chekov's first name?","What furry creatures nearly overwhelmed the Enterprise?","Which cast member was born in Toccoa Georgia?","What actress played Spock's mother?","The main enery weapons were called?","What is Chief Engineer Scott's first name?"],
+    answers: [
+        ["Jonathan", "Tiberius", "Kelley", "Montgomery"],["Excelsior", "Yorktown", "Yamoto", "Enterprise"],["Romulans", "Ferengi", "Cylons", "Daveks"],["Pavel", "Vladimir", "Rasputin", "Leo"],["Ewoks", "Furbys", "Tribbles", "Urchins"],["DeForest Kelley","Nichelle Nichols","Jimmie Doohan","George Takei"],["Christine Lahti","Betty White","Jane Mansfield","Jane Wyatt"],["Lasers","Phasers","Ion Cannons","Disruptors"],["Montgomery","Gentry","George","Christopher"]
+        ],
+    correctAnswer: [
+        "B. Tiberius","D. Enterprise","A. Romulans","A. Pavel","C. Tribbles","A. DeForest Kelley","D. Jane Wyatt","B. Phasers","A. Montgomery"] 
+    };
 
 var kudoArray = ["Good job!","Awesome!","You rock!","A true Trekkie!","Perfect!","Great!","Sweet!"];
 
@@ -101,7 +75,7 @@ function outtaTime() {
   var dispImg = "<img src='assets/images/ST" + questionNum + ".jpg' class='rounded mx-auto d-block gamePhoto'>";
 
   gameHTML = 
-      "<p class='text-center'>Time's up! The correct answer is:  " + correctAnswer[questionNum] + "</p>" + dispImg;
+      "<p class='text-center'>Time's up! The correct answer is:  " + gameArray.correctAnswer[questionNum] + "</p>" + dispImg;
   
   $(".gameScreen").html(gameHTML);
   setTimeout(pauseGame, 4000); 
@@ -113,7 +87,7 @@ function gameWin() {
   kudoRsp = kudoArray[Math.floor(Math.random() * kudoArray.length)];
 
   gameHTML = 
-      "<p class='text-center'>" + correctAnswer[questionNum] + " is correct! " + kudoRsp + "</p>" + 
+      "<p class='text-center'>" + gameArray.correctAnswer[questionNum] + " is correct! " + kudoRsp + "</p>" + 
       dispImg; 
 
     $(".gameScreen").html(gameHTML);
@@ -125,7 +99,7 @@ function gameLose() {
   var dispImg = "<img src='assets/images/ST" + questionNum + ".jpg' class='rounded mx-auto d-block gamePhoto'>";
 
   gameHTML = 
-      "<p class='text-center'>Sorry, the correct answer is: " + correctAnswer[questionNum] + "</p>" + 
+      "<p class='text-center'>Sorry, the correct answer is: " + gameArray.correctAnswer[questionNum] + "</p>" + 
       dispImg;
   
   $(".gameScreen").html(gameHTML);
@@ -133,7 +107,7 @@ function gameLose() {
 }
 
 function pauseGame() {
-  if (questionNum < questionsArray.length-1) {
+  if (questionNum < gameArray.questions.length-1) {
       questionNum++;
       generateHTML();
       seconds = 15;
@@ -153,11 +127,11 @@ function pauseGame() {
 function generateHTML() {
 
   gameHTML = 
-      "<p class='text-center timer-disp'>Time Remaining: <span class='timer'>15</span></p><p class='text-center'>" + "Question " + (questionNum + 1) + ": " + questionsArray[questionNum] + "</p>" +
-      "<p class='answer'>A. " + answersArray[questionNum][0] + "</p>" +
-      "<p class='answer'>B. " + answersArray[questionNum][1] + "</p>" +
-      "<p class='answer'>C. " + answersArray[questionNum][2] + "</p>" +
-      "<p class='answer'>D. " + answersArray[questionNum][3] + "</p>";
+      "<p class='text-center timer-disp'>Time Remaining: <span class='timer'>15</span></p><p class='text-center'>" + "Question " + (questionNum + 1) + ": " + gameArray.questions[questionNum] + "</p>" +
+      "<p class='answer'>A. " + gameArray.answers[questionNum][0] + "</p>" +
+      "<p class='answer'>B. " + gameArray.answers[questionNum][1] + "</p>" +
+      "<p class='answer'>C. " + gameArray.answers[questionNum][2] + "</p>" +
+      "<p class='answer'>D. " + gameArray.answers[questionNum][3] + "</p>";
 
   $(".gameScreen").html(gameHTML);
 
